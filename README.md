4bd88716a1cfa18f4827277018440d5085eb434b
Steve <steve@whtly.com>
2014-09-05 19:40:06 +0100

    Committing version 0.1.
    It all begins here...

---
+++ README.md
@@ -0,0 +1,81 @@
+jQuery Teletype Plugin
+======================
+
+Teletype is a jQuery plugin that types out text, and then optionally deletes it, replicating human interaction.
+
+Additional options provide the ability to preserve the typed text, in a console / terminal format, pause during typing and delete characters.
+
+Installation
+---
+
+    <script src="jquery.js"></script>
+	<script src="jquery.teletype.js"></script>
+	<script>
+		$( function() {
+			$( '.type-text' ).teletype( {
+				text: [ 'one', 'two', 'three' ],
+				typeDelay: 0,
+				backDelay: 20
+			} );
+		} );
+	</script>
+	...
+	<div class="type-text"></div>
+
+Options
+-------
+
+   
+ Option     | Default     | Description
+------------|-------------|------------
+ text       | `['one','two','three']` (array) | List of strings to output.     
+ typeDelay  | `100` (integer)                     | Minimum delay, in ms, between typing characters.    
+ backDelay  | `50` (integer)                      | Minimum delay, in ms, between deleting characters.
+ blinkSpeed | `1000` (integer)                    | Interval, in ms, that the cursor will flash.
+ cursor     | <code>"&#124;"</code> (string)      | Character used to represent the cursor.
+ delay      | `2000` (int)                        | Time in ms to pause before deleting the current text.
+ preserve   | `false` (boolean)                   | Prevent auto delete of the current string and begin outputting the next string.
+ prefix     | `""` (string)                       | Begin each string with this prefix value.
+ loop       | `0` (int)                           | Number of times to loop through the output strings, for unlimited use `0`.
+
+Deleting characters `~`
+---
+
+It is possible to delete a defined number of characters then proceed with the rest of the text output. 
+
+Use `~x ` within the text string, where x is an integer value defining the characters to backspace, followed by a space.
+
+Example, type "auti", delete 1 character and continue to type "o", resulting in the word "auto":
+
+```
+auti~1 o^`
+```
+
+Pause / Delay `^`
+---
+
+Delay typing the next character using `^x ` where x in an integer value of milliseconds to pause.
+
+To pause for 2 seconds after typing the word "pause" before continuing to type: 
+
+```
+pause^2000 more
+```
+
+Line Breaks `\n`
+---
+
+Line breaks can be added using `\n`, which are converted to `<br />` during output.
+
+Generated Markup
+----------------
+
+The following markup is used to output the teletype text.
+
+```
+<span class="teletype-prefix">[prefix]</span>
+<span class="teletype-text">[string]</span>
+<span class="teletype-cursor">[cursor]</span>
+```
+    
+This provides the ability to customise the style of the output text in your CSS.
---
+++ jquery.teletype.js
@@ -0,0 +1,136 @@
+/*
+* @fileOverview Teletype - jQuery Plugin
+* @version 0.1
+*
+* @author Steve Whiteley http://stv.whtly.com
+* @see https://github.com/stvwhtly/jquery-teletype-plugin
+*
+* Copyright (c) 2014 Steve Whiteley
+* Dual licensed under the MIT or GPL Version 2 licenses.
+*
+*/
+( function ( $ ) {
+	$.fn.teletype = function( options ) {
+		var settings = $.extend( {}, $.fn.teletype.defaults, options );
+		var self = $( this ),
+			output = null,
+			current = { 
+				string: '',
+				index: 0,
+				position: 0,
+				loop: 0
+			};
+		var next = function() {
+			current.index++;
+			if ( current.index >= settings.text.length ) {
+				current.index = 0;
+				current.loop++;
+				if ( settings.loop != false && ( settings.loop == current.loop ) ) {
+					return false;
+				}
+			}
+			current.position = 0;
+			current.string = settings.text[current.index];
+			return true;
+		};
+		var type = function() {
+			if ( settings.prefix && current.position == 0 ) {
+				if ( current.loop == 0 && current.index == 0 ) {
+					$( '<span />' ).addClass( 'teletype-prefix' ).html( settings.prefix ).prependTo( self );
+				}
+			}
+			var letters = current.string.split( '' ),
+				letter = letters[current.position];
+			if ( letter == '^' || letter == '~' ) {
+				var end = current.string.substr( current.position + 1 ).indexOf( ' ' );
+				var value = current.string.substr( current.position + 1, end );
+				if ( $.isNumeric( value ) ) {
+					current.string = current.string.replace( letter + value + ' ', '' );
+					if ( letter == '^' ) {
+						window.setTimeout( function() {
+							window.setTimeout( type, delay( settings.typeDelay ) );
+						}, value );
+					} else {
+						var index = current.position - value;
+						current.string = current.string.substr( 0, index - 1 ) + current.string.substr( current.position - 1 );
+						window.setTimeout( function() {
+							backspace( Math.max( index, 0 ) );
+						}, delay( settings.backDelay ) );
+					}
+					return;
+				}
+			} else if ( letter == '\\' ) {
+				var nextChar = current.string.substr( current.position + 1, 1 );
+				if ( nextChar  == 'n' ) {
+					current.position++;
+					letter = '<br />';
+				}
+			}
+			output.html( output.html() + letter );
+			current.position++;
+			if ( current.position < current.string.length ) {
+				window.setTimeout( type, delay( settings.typeDelay ) );
+			} else if ( settings.preserve == false ) {
+				window.setTimeout( function() {
+					window.setTimeout( backspace, delay( settings.backDelay ) );
+				}, settings.delay );
+			} else {
+				output.html( output.html() + '<br />' + '<span class="teletype-prefix">' + settings.prefix + '</span>' );
+				if ( next() ) {
+					window.setTimeout( function() {
+						window.setTimeout( type, delay( settings.typeDelay ) );
+					}, settings.delay );
+				}
+			}
+		};
+		var backspace = function( stop ) {
+			if ( !stop ) {
+				stop = 0;
+			}
+			if ( current.position > stop ) {
+				output.html( output.html().slice( 0, -1 ) );
+				window.setTimeout( function() {
+					backspace( stop );
+				}, delay( settings.backDelay ) );
+				current.position--;
+			} else {
+				if ( stop == 0 ) {
+					if ( next() == false ) {
+						return;
+					}
+				}
+				window.setTimeout( type, delay( settings.typeDelay ) );
+			}
+		};
+		var delay = function( speed ) {
+			return Math.floor( Math.random() * 200 ) + speed;
+		};
+		return this.each( function() {
+			current.string = settings.text[current.index];
+			self.addClass( 'teletype' ).empty();
+			output = $( '<span />' ).addClass( 'teletype-text' ).appendTo( self );
+			
+			if ( settings.cursor ) {
+				var cursor = $( '<span />' )
+					.text( settings.cursor )
+					.addClass( 'teletype-cursor' )
+					.appendTo( self );
+				setInterval ( function() {
+					cursor.animate( { opacity: 0 } ).animate( { opacity: 1 } );
+				}, settings.blinkSpeed );
+			}
+			type();
+		} );
+	};
+	$.fn.teletype.defaults = {
+		text: [ 'one', 'two', 'three' ],
+		typeDelay: 100,
+		backDelay: 50,
+		blinkSpeed: 1000,
+		delay: 2000,
+		cursor: '|',
+		preserve: false,
+		prefix: '',
+		loop: 0
+	};
+}( jQuery ) );
